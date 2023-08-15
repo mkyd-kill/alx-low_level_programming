b92043a7de82798ff29b37090baf8004ae5523db
mkyd-kill <romeomureithi@gmail.com>
2023-08-15 09:40:42 -0700

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - returns absolute value of a number
+ * @n: The number
+ * Return: The absolute number
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
