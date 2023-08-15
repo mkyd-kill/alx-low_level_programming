a6d22576c64e577ee3fb94a5833a715706a08a05
mkyd-kill <romeomureithi@gmail.com>
2023-08-15 08:35:15 -0700

    _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - program entry
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_puthcar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_puthcar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
