5457b3d8d8b8d87537af060e069ce982bf2456c5
mkyd-kill <romeomureithi@gmail.com>
2023-08-15 08:50:41 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,15 @@
 /**
  * print_alphabet - prints the alphabets
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void
  */
 void print_alphabet(void)
 {
-	for (char ch = 'a'; ch <= 'z'; ch++)
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
 	_putchar('\n');
-	return (0);
 }
