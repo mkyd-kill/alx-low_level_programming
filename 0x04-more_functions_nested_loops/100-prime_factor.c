0a8c6aa12bba31e9ab3e94594483e09d8eb23324
mkyd-kill <romeomureithi@gmail.com>
2023-08-17 11:08:00 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - prints the largest prime factor of a number
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	int x, max;
+	long numb = 612852475143;
+	double square = sqrt(num);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (numb % x == 0)
+		{
+			max = numb / x;
+		}
+	}
+	printf("%d\n", max);
+
+	return (0);
+}
