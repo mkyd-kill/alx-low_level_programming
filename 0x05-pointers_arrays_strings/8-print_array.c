d2cea5092bc580cc019d672e0f8ba9ff1bb2a738
mkyd-kill <romeomureithi@gmail.com>
2023-08-21 05:38:18 -0700

    updated arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (i != n)
+		printf("%d", a[i]);
+		if (i < n - 1)
 		{
-			printf("%d, ", a[i]);
-		}
-		else
-		{
-			printf("%d\n", a[i]);
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
