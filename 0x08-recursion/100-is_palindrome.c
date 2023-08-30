002ef4d34c4cadec29f9674823d17ff2e5231b5c
mkyd-kill <romeomureithi@gmail.com>
2023-08-30 09:55:13 -0700

    palidrome checker

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_palidrome - checks if a string is a palidrome
+ * @s: The string
+ * Return: 1 if yes 0 otherwise
+ */
+int is_palidrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	int i, int j;
+
+	for (i = 0; j = len - 1; i < j; i++, j--)
+	{
+		if (s[i] != s[j])
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
