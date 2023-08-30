052da1bb8cacb1a722d8e9a0a28d41e2c7ba46a8
mkyd-kill <romeomureithi@gmail.com>
2023-08-30 10:22:15 -0700

    fixed linking

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,16 @@
 #include "main.h"
 /**
+ * _strlen_recursion - returns the length of a string
+ * @s: The string
+ * Return: The length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+/**
  * is_palidrome - checks if a string is a palidrome
  * @s: The string
  * Return: 1 if yes 0 otherwise
