55242cf3df964d0ebadf1913179e3ee28b6d5182
mkyd-kill <romeomureithi@gmail.com>
2023-08-30 10:38:23 -0700

    renamed values

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,13 +17,13 @@ int _strlen_recursion(char *s)
  * @e: End
  * Return: 1 if yes 0 otherwise
  */
-int check_pal(char *s, int s, int e)
+int check_pal(char *s, int start, int end)
 {
-	if (n >= e)
+	if (start >= end)
 		return (1);
-	if (s[n] != s[e])
+	if (s[start] != s[end])
 		return (0);
-	return (check_pal(s, n + 1, e - 1));
+	return (check_pal(s, start + 1, end - 1));
 }
 /**
  * is_palidrome - checks if a string is a palidrome
