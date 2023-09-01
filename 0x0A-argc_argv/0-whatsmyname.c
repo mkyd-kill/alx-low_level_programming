c3425fe2c280c66e15b0ccc306267ec31199f8d6
mkyd-kill <romeomureithi@gmail.com>
2023-09-01 09:20:59 -0700

    file argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its own name
+ * @argc: Number of arguments passed
+ * @argv: Array of arguments
+ * Return: 0 success 1 otherwise
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# File argc, **argv
