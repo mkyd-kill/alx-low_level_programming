a12f5bbb9792d5dcae782bd3fb1719ce8db1ed31
mkyd-kill <romeomureithi@gmail.com>
2023-09-02 23:03:06 -0700

    change update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,29 +17,24 @@ int main(int argc, char **argv)
 	}
 	c = atoi(argv[1]);
 	if (c < 0)
-		printf("0\n");
-	while (c > 0)
 	{
-		switch (c)
-		{
-			case 25:
-				c -= 25;
-				break;
-			case 10:
-				c -= 10;
-				break;
-			case 5:
-				c -= 5;
-				break;
-			case 2:
-				c -= 2;
-				break;
-			case 1:
-				c -= 1;
-				break;
-			default:
-				break;
-		}
+		printf("0\n");
+		return (0);
+	}
+	for (; c >= 0;)
+	{
+		if (c >= 25)
+			c -= 25;
+		else if (c >= 10)
+			c -= 10;
+		else if (c >= 5)
+			c -= 5;
+		else if (c >= 2)
+			c -= 2;
+		else if (c >= 1)
+			c -= 1;
+		else
+			break;
 		coins += 1;
 	}
 	printf("%d\n", coins);
