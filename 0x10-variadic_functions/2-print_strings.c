62eac9883302b7a0be1fa62ace3701b5c941230b
mkyd-kill <romeomureithi@gmail.com>
2023-09-14 19:35:19 -0700

    updated variable declaration

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,11 +11,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	char *string;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
+		char *string;
+
 		string = va_arg(list, char *);
 
 		if (string == NULL)
