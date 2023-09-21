a8e567b73a0b5916190b9b2b1940ae61614e7cea
mkyd-kill <romeomureithi@gmail.com>
2023-09-21 15:32:09 -0700

    printing elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - prints elements of a linked list
+ * @h: The linked list array
+ * Return: The node number
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+	
+	if (h == NULL)
+		return (0);
+	do
+	{
+		if (h->str == NULL)
+			printf("[%u] (nil)\n", h->len);
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		i += 1;
+		h = h->next;
+	} while (h != NULL);
+	return (i);
+}
