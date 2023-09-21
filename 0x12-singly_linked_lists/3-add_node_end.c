a433e9f08ab6ea6b3c7c2e03c8e02618bb058ecd
mkyd-kill <romeomureithi@gmail.com>
2023-09-21 16:19:56 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,7 +26,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	list_t *last = *head;
 
-	if (str == NULL || new_node == NULL)
+	if (new_node == NULL || str == NULL)
 	{
 		free(new_node);
 		return (NULL);
