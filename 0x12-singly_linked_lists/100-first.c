56663f187550898efd77710543ec79b3653f83cd
mkyd-kill <romeomureithi@gmail.com>
2023-09-22 05:10:21 -0700

    using the __attribute__ to execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * A constructor that executes before main
+ */
+void _print_first(void)__attribute__((constructor));
+/**
+ * _print_first - prints a text before main
+ */
+void _print_first(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
