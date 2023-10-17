42ec8b1f4faea8410746eb0255df8bfe1bf15e76
mkyd-kill <romeomureithi@gmail.com>
2023-10-17 10:49:45 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '\0' && b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 	for (i = i - 1; i >= 0; i--)
