07a120fc0826095813d086ede89e2522af9f77a3
mkyd-kill <romeomureithi@gmail.com>
2023-10-20 15:47:39 +0300

    updated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define _FUNCTION_POINTERS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
